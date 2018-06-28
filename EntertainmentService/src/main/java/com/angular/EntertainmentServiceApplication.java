591493901918a8980d1e009be1433cf3388d7a71
esubbu <e.subbu23@gmail.com>
2018-06-28 22:02:23 +0530

    updated pom.xml

--- EntertainmentService/src/main/java/com/angular/EntertainmentServiceApplication.java
+++ EntertainmentService/src/main/java/com/angular/EntertainmentServiceApplication.java
@@ -2,10 +2,11 @@ package com.angular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com")
 @SpringBootApplication
-public class EntertainmentServiceApplication extends SpringBootServletInitializer {
+public class EntertainmentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EntertainmentServiceApplication.class, args);
