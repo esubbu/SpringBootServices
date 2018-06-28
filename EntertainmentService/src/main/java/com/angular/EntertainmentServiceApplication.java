cd2118bd861b0fc4099d09526c288cde979d026d
esubbu <e.subbu23@gmail.com>
2018-06-28 21:20:33 +0530

    added tomcat context

--- EntertainmentService/src/main/java/com/angular/EntertainmentServiceApplication.java
+++ EntertainmentService/src/main/java/com/angular/EntertainmentServiceApplication.java
@@ -2,9 +2,10 @@ package com.angular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EntertainmentServiceApplication {
+public class EntertainmentServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EntertainmentServiceApplication.class, args);
