24d9dc48f5f266f590e231576b7514b5c6178c7b
esubbu <e.subbu23@gmail.com>
2018-07-04 12:56:51 +0530

    Added Logger functionality..

--- EntertainmentService/src/main/java/com/angular/controllers/MovieRestController.java
+++ EntertainmentService/src/main/java/com/angular/controllers/MovieRestController.java
@@ -1,8 +1,9 @@
 package com.angular.controllers;
 
-import java.awt.PageAttributes.MediaType;
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.angular.models.Movie;
@@ -21,13 +21,15 @@ import com.angular.models.Movie;
 @RestController
 public class MovieRestController {
 	
-	
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieRestController.class);
+
 	@Autowired
 	MongoTemplate mongoTemplate;
 
 	@CrossOrigin(origins = "*")
 	@PostMapping(path="/insertMovie",consumes = "application/json")
 	public String insertMovie(@RequestBody Movie movie){
+		LOGGER.info("insertMovie action called..");
 		mongoTemplate.save(movie);
 		return "successfully inserted sd!!";
 		
@@ -46,6 +48,8 @@ public class MovieRestController {
 	@CrossOrigin(origins = "*")
 	@GetMapping(path="/getMovies",produces="application/json")
 	public ArrayList<Movie> getMovies(){
+		LOGGER.info("getMovies action called..");
+
 		ArrayList<Movie> movie=(ArrayList<Movie>) mongoTemplate.findAll(Movie.class);
 		return movie;
 		
@@ -53,6 +57,7 @@ public class MovieRestController {
 	
 	@GetMapping(path="/welcome")
 	public String getWelcomeMsg(){
+		LOGGER.info("getWelcomeMsg action called..");
 		return "Welcome!!!!!!!!!!! Angular services New";
 		
 	}
